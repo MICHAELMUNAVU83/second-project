4c1820ed7869544bd1574b93e1448efded83d7e5
trance <michaelmunavu83@gmail.com>
2021-07-18 08:46:04 +0300

    put all as alert

--- ps.js
+++ ps.js
@@ -29,20 +29,83 @@ function woman(){
         let DD =parseInt( document.getElementById('day').value ) ;
         let kim =  ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
         let sum = Math.floor(kim);
-        
+        let maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+        let femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+        let weekdays = ['Sunday','Monday','Tuesday', 'Wednesday','Thursday', 'Friday', 'Saturday'];
+  
         
         alert(sum)
+
+
         let gen = document.getElementById('resu').innerHTML;
-        if(sum == 2 && gen== "male"){
-           alert('right')
+        if(sum ==  0 && gen== "female"){
+           alert(femaleNames[0])
         }
+        else if (sum == 1 && gen == "female") {
+           alert(femaleNames[1])
+           
+        }
+        else if (sum ==  2 && gen == "female") {
+         alert(femaleNames[2])
+         
+      }
+      else if (sum == 3 && gen == "female") {
+         alert(femaleNames[3])
+         
+      }
+     
+      else if (sum == 4 && gen == "female") {
+         alert(femaleNames[0])
+         
+      }
+     
+      else if (sum == 5 && gen == "female") {
+         alert(femaleNames[5])
+         
+      }
+     
+      else if (sum == 6 && gen == "female") {
+         alert(femaleNames[6])
+         
+      }
+
+      if(sum == 0 && gen == "male"){
+         alert(maleNames[0])
+      }
+      else if (sum == 1 && gen == "male") {
+         alert(maleNames[1])
+         
+      } 
+      else if (sum == 2 && gen == "male") {
+         alert(maleNames[2])
+         
+      } 
+      else if (sum == 3 && gen == "male") {
+         alert(maleNames[3])
+         
+      } 
+      else if (sum == 4 && gen == "male") {
+         alert(maleNames[4])
+         
+      } 
+      else if (sum == 5  && gen == "male") {
+         alert(maleNames[5])
+         
+      } 
+      else if (sum == 6 && gen == "male") {
+         alert(maleNames[6])
+         
+      } 
+   }
+     
+     
        
 
          
 
 
       
-      }  
+      
       
 
       
@@ -58,30 +121,8 @@ function woman(){
 
          
             
-        let maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
-        let femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
-        let weekdays = ['Sunday','Monday','Tuesday', 'Wednesday','Thursday', 'Friday', 'Saturday'];
-  
-            
-
-
-
-
         
-
-
-       
-
-       
-
-         
-
-       
-    
-
-    
- 
-
+            
 
 
 
