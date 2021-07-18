f590f0d12d390d3b7081e52211aaf2702a5695b8
trance <michaelmunavu83@gmail.com>
2021-07-18 08:11:03 +0300

    started again ifs

--- ps.js
+++ ps.js
@@ -20,7 +20,7 @@ function woman(){
 
 
     function mwaka(){
-        let main = document.getElementById('result').value
+        
         let year = document.getElementById('year').value
         let full = String(year)
         let CC = parseInt(full.substring(0, 2));
@@ -29,7 +29,24 @@ function woman(){
         let DD =parseInt( document.getElementById('day').value ) ;
         let kim =  ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
         let sum = Math.floor(kim);
-        alert(sum);}    
+        
+        
+        alert(sum)
+        let gen = document.getElementById('resu').innerHTML;
+        if(sum == 2 && gen== "male"){
+           alert('right')
+        }
+       
+
+         
+
+
+      
+      }  
+      
+
+      
+
             
             
         
@@ -39,12 +56,25 @@ function woman(){
         
 
 
-        
+         
+            
         let maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
         let femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
         let weekdays = ['Sunday','Monday','Tuesday', 'Wednesday','Thursday', 'Friday', 'Saturday'];
   
+            
 
+
+
+
+        
+
+
+       
+
+       
+
+         
 
        
     
